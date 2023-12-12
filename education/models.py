cd6ab74cf39b5146ed0a3810e0d3f403cbbbf9eb
Sergey <smirsand@mail.ru>
2023-12-12 21:40:49 +0300

    add files education

---
+++ education/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Chapter(models.Model):
+    """
+    Модель учебного раздела.
+    """
+    name_chapter = models.CharField(max_length=100, verbose_name='наименование раздела')
+    description_chapter = models.TextField(verbose_name='описание раздела', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name_chapter}'
+
+    class Meta:
+        verbose_name = 'раздел'
+        verbose_name_plural = 'разделы'
+
+
+class Materials(models.Model):
+    """
+    Модель учебного материала.
+    """
+    name_material = models.CharField(max_length=255, verbose_name='название')
+    description_material = models.TextField(verbose_name='описание')
+    video_link_material = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name_material}'
+
+    class Meta:
+        verbose_name = 'материал'
+        verbose_name_plural = 'материалы'
