25c3b1a645766bbc873fd03efeacaf9a7097f8b6
Sergey <smirsand@mail.ru>
2023-12-13 16:58:02 +0300

    add views.py education

--- education/views.py
+++ education/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from education.models import Chapter, Materials
 
 
-def index(request):
-    return render(request, 'education/index.html')
+class ChapterListView(ListView):
+    model = Chapter
+    template_name = 'education/home.html'
+    extra_context = {'title': 'Главная'}
+
+
+class ChapterDetailView(DetailView):
+    model = Chapter
+    template_name = 'education/education_detail.html'
+    extra_context = {'title': 'Материалы раздела'}
+
+
+class MaterialsListView(ListView):
+    model = Materials
+    template_name = 'education/education_detail.html'
+
+    def get_queryset(self):
+        chapter_id = self.kwargs['pk']
+        return Materials.objects.filter(chapter_id=chapter_id)
