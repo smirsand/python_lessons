2d9531669657765ee264eae158e0f5567bb290bd
Sergey <smirsand@mail.ru>
2023-12-15 13:14:55 +0300

    add code admin.py education

--- education/admin.py
+++ education/admin.py
@@ -1,17 +1,31 @@
 from django.contrib import admin
 
-from education.models import Chapter, Materials
+from education.models import Chapter, Material, Test, Answer
 
 
 @admin.register(Chapter)
 class ChapterAdmin(admin.ModelAdmin):
-    list_display = ('name_chapter', 'description_chapter',)  # Вывод столбцов в таблице
+    list_display = ('name_chapter',)  # Вывод столбцов в таблице
     list_filter = ('name_chapter',)  # Фильтрация
     search_fields = ('name_chapter',)  # Поиск
 
 
-@admin.register(Materials)
-class MaterialsAdmin(admin.ModelAdmin):
-    list_display = ('name_material', 'description_material',)  # Вывод столбцов в таблице
-    list_filter = ('name_material',)  # Фильтрация
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('name_material', 'chapter',)  # Вывод столбцов в таблице
+    list_filter = ('name_material', 'chapter',)  # Фильтрация
     search_fields = ('name_material',)  # Поиск
+
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('question',)  # Вывод столбцов в таблице
+    list_filter = ('question',)  # Фильтрация
+    search_fields = ('question',)  # Поиск
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('test', 'text', 'is_correct')  # Вывод столбцов в таблице
+    list_filter = ('text', 'test', 'is_correct',)  # Фильтрация
+    search_fields = ('text', 'test', 'is_correct',)  # Поиск
