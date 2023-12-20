98a40af98f834440b92bc03ee0bb464ac5181067
Sergey <smirsand@mail.ru>
2023-12-21 00:46:25 +0300

    add code views.py education

--- education/views.py
+++ education/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import DetailView, ListView
 
-from education.models import Chapter, Material, Test
+from education.forms import TestResultForm, TestForm
+from education.models import Chapter, Material, Test, TestResult
 
 
 class ChapterListView(ListView):
@@ -11,13 +14,13 @@ class ChapterListView(ListView):
 
 class ChapterDetailView(DetailView):
     model = Chapter
-    template_name = 'education/education_detail.html'
+    template_name = 'education/materials_list.html'
     extra_context = {'title': 'Материалы раздела'}
 
 
 class MaterialListView(ListView):
     model = Material
-    template_name = 'education/education_detail.html'
+    template_name = 'education/materials_list.html'
     extra_context = {'title': 'Список материалов раздела'}
 
     def get_queryset(self):
@@ -31,7 +34,31 @@ class MaterialDetailView(DetailView):
     extra_context = {'title': 'Материал'}
 
 
-class TestListView(ListView):
+# @login_required
+class TestDetailView(DetailView):
     model = Test
-    template_name = 'education/test_list.html'
-    extra_context = {'title': 'Тесты'}
+    form_class = TestForm
+    template_name = 'education/test_detail.html'
+    extra_context = {'title': 'Тест'}
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = TestResultForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        test_id = kwargs['pk']
+        test = self.get_object()
+        answer = request.POST.get('answer', '')
+        user = request.user
+        TestResult.objects.create(user=user, test=test, choice=answer)
+        next_tests = Test.objects.filter(order__gt=test.order).order_by('order')
+        next_test = next_tests.first()
+
+        if next_test:
+            return HttpResponseRedirect(reverse('education:test_detail', args=[next_test.pk]))
+        else:
+            return HttpResponseRedirect(reverse('education:list_chapter'))
