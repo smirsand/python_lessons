71155133d7e5e165412621b5c3436fb86c435ff0
Sergey <smirsand@mail.ru>
2024-01-01 13:37:41 +0300

    add code views.py users

--- users/views.py
+++ users/views.py
@@ -9,16 +9,25 @@ from users.models import User
 
 
 class LoginView(BaseLoginView):
+    """
+    Класс для отображения страницы входа пользователя.
+    """
     template_name = 'users/login.html'
     success_url = reverse_lazy("users:login")
 
 
 class LogoutView(BaseLogoutView):
+    """
+    Класс для отображения страницы выхода пользователя.
+    """
     template_name = 'users/login.html'
     success_url = reverse_lazy("education:home")
 
 
 class RegisterView(CreateView):
+    """
+    Класс для отображения страницы регистрации пользователя.
+    """
     model = User
     form_class = UserRegisterForm
     template_name = 'users/register.html'
@@ -26,6 +35,9 @@ class RegisterView(CreateView):
 
 
 class ProfileView(LoginRequiredMixin, UpdateView):
+    """
+    Класс для отображения страницы профиля пользователя.
+    """
     model = User
     form_class = ProfileForm
     success_url = reverse_lazy('users:profile')
