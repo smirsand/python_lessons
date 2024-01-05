9f5e8da990506c15be0dd59d35ae73dadacc03fa
Sergey <smirsand@mail.ru>
2024-01-05 19:12:45 +0300

    add code urls.py education

--- education/urls.py
+++ education/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 
 from education.apps import EducationConfig
 from education.views import ChapterListView, MaterialListView, MaterialDetailView, TestDetailView, TestResultListView, \
-    TestListView, ChapterRetrieveAPIView, MaterialRetrieveAPIView, TestRetrieveAPIView, TestResultRetrieveAPIView, \
-    TestResultListAPIView, TestResultCreateAPIView
+    TestListView, MaterialRetrieveAPIView, TestRetrieveAPIView, TestResultListAPIView, \
+    TestResultCreateAPIView, ChapterCreateAPIView, ChapterListAPIView
 
 app_name = EducationConfig.name
 
 urlpatterns = [
     path('', ChapterListView.as_view(), name='list_chapter'),
-    path('chapter/<int:pk>/', ChapterRetrieveAPIView.as_view(), name='chapter_retrieve'),
+    path('chapter/create/', ChapterCreateAPIView.as_view(), name='chapter_create'),
+    path('chapter_list/', ChapterListAPIView.as_view(), name='chapter_list'),
     path('list/<int:pk>/', MaterialListView.as_view(), name='list_materials'),
     path('material/<int:pk>/', MaterialDetailView.as_view(), name='material'),
     path('material/<int:pk>/', MaterialRetrieveAPIView.as_view(), name='material_retrieve'),
@@ -19,5 +20,4 @@ urlpatterns = [
     path('result/create/', TestResultCreateAPIView.as_view(), name='result_create'),
     path('result_list/', TestResultListView.as_view(), name='list_result'),
     path('results_list/<int:pk>/', TestResultListAPIView.as_view(), name='list_results'),
-    path('result/<int:pk>/', TestResultRetrieveAPIView.as_view(), name='result_retrieve'),
 ]
